fa77fa44159504a87efcd45bed4d7f680396e8a9
NigamLad <nigamlad2012@gmail.com>
2021-09-23 14:56:26 -0700

    Added Random Sort
    Code added for random sort,
    RNG function
    Shuffle array function

--- lotsOfSorting.java
+++ lotsOfSorting.java
@@ -30,11 +30,67 @@ public class lotsOfSorting {
 
 	}
 
-	// method for third sorting algorithm
-	public static int[] sort3(int[] a) {
+	// Random Sort
+	// Stops after 100 000 attempt
+	public static int[] randomSort(int[] a) {
+
+		int counter = 0;
+		
+		while(counter < 100000 && checkSequential(a) == false) {
+			a = shuffle(a);
+			counter++;
+		}
+		
+		if(counter == 100000) {
+			System.out.println("Trials exceeded 100 000");
+		}else {
+			System.out.println("Array sorted");
+		}
 
 		return a;
 
 	}
+
+	//Random Number Generator
+	public static int RNG(int min, int max) {
+		int random = (int) (Math.random() * (max - min + 1) + min);
+		return random;
+	}
+
+	//Check if the array is in order
+	public static boolean checkSequential(int[] a) {
+		boolean sequential = true;
+		int size = a.length;
+		
+		for (int i = 0; i < size; i++) {
+			if(a[i] != i + 1) {
+				sequential = false;
+			}	
+		}
+		
+		return sequential;
+	}
+
+	public static int[] shuffle(int[] a) {
+		int rand1 = 0;
+		int rand2 = 0;
+		int size = a.length;
+
+		for (int i = 0; i < (size * size); i++) {
+			int temp1 = 0;
+			int temp2 = 0;
+
+			rand1 = RNG(0, (size - 1));
+			rand2 = RNG(0, (size - 1));
+
+			temp1 = a[rand1];
+			temp2 = a[rand2];
+			a[rand1] = temp2;
+			a[rand2] = temp1;
+		}
+		return a;
+	}
+
+	
 
 }
