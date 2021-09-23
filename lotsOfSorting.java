f805999218f5d1ce8c7e133d4391bdb9d84e3ab5
Ben Wilson <benwilsonthehuman@gmail.com>
2021-09-22 18:59:56 -0700

    created quick skeleton code for the exercise

---
+++ lotsOfSorting.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class lotsOfSorting {
+
+	
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * To Do:
+		 * 1. Input a list of integers to be sorted
+		 * 2. Simple menu to decide which sorting alg to use on the list	
+		 * 3. Returns sorted list and time to sort using the chosen sorting alg
+		 */
+
+	}
+
+	// method for first sorting algorithm
+	public static int[] sort1(int[] a) {
+
+		return a;
+
+	}
+
+	// method for second sorting algorithm
+	public static int[] sort2(int[] a) {
+
+		return a;
+
+	}
+
+	// method for third sorting algorithm
+	public static int[] sort3(int[] a) {
+
+		return a;
+
+	}
+
+}
