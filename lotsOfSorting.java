316c96159484dea0d949a6a319d31b328ae22cc9
yiduguo-hp <yidu.guo@outlook.com>
2021-09-23 20:21:06 -0700

    fixed bracket x2

--- lotsOfSorting.java
+++ lotsOfSorting.java
@@ -64,17 +64,17 @@ public class lotsOfSorting {
     //bubblesort
 		int n = a.length;
 		int temp = 0;
-		for(int i=0; i < n; i++){
-			for(int j=1; j < (n-i); j++){
-				if(a[j-1] > a[j]){
+		for(int i=0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if (a[j - 1] > a[j]) {
 					//swap elements
-					temp = a[j-1];
-					a[j-1] = a[j];
+					temp = a[j - 1];
+					a[j - 1] = a[j];
 					a[j] = temp;
 				}
 
 			}
-
+		}
 		return a;
 
 	}
